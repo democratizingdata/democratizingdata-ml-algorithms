eedb8a14cdda28817bb0618c1b216cfb344e1a5a
Ryan Hausen <ryan.hausen@gmail.com>
2022-12-12 22:18:48 -0500

    kaggle repository working

--- src/data/kaggle_repository.py
+++ src/data/kaggle_repository.py
@@ -25,7 +25,7 @@ class KaggleRepository(Repository):
             )
         )
 
-        text_dict = dict(Id=[], texts=[])
+        text_dict = dict(Id=[], text=[])
         train_files_dir = os.path.join(
             os.path.dirname(__file__),
             "../../data/kaggle/train"
@@ -33,14 +33,13 @@ class KaggleRepository(Repository):
         for f in os.listdir(train_files_dir):
             with open(os.path.join(train_files_dir, f), "r") as f:
                 json_data = json.load(f)
-                text_dict["texts"].append(" ".join([l["text"].replace("\n", "").strip() for l in json_data]))
+                text_dict["text"].append(" ".join([l["text"].replace("\n", "").strip() for l in json_data]))
                 text_dict["Id"].append(os.path.basename(f.name).split(".")[0])
 
         df_texts = pd.DataFrame(text_dict)
-        print(df_texts.head(5))
         df = pd.merge(df_labels, df_texts, on="Id")
 
-        return iter([df.loc[:, ["Id", "texts", "dataset_label"]],])
+        return iter([df.loc[:, ["Id", "text", "dataset_label"]],])
 
     def get_test_data_dataframe(self, batch_size: int) -> Iterable[pd.DataFrame]:
         raise NotImplementedError()
@@ -49,5 +48,5 @@ class KaggleRepository(Repository):
 
 if __name__=="__main__":
     repo = KaggleRepository()
-    df = repo.get_training_data_dataframe(1)
+    df = next(repo.get_training_data_dataframe(1))
     print(df)
