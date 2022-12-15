6a05580165266831e9fb718e6a63311bbe1c7871
ryanhausen <rhausen@ucsc.edu>
2022-12-15 17:01:17 -0500

    added stubs for mypy. No mypy errors

---
+++ stubs/thefuzz/string_processing.pyi
@@ -0,0 +1,12 @@
+from _typeshed import Incomplete
+
+PY3: Incomplete
+string = str
+
+class StringProcessor:
+    regex: Incomplete
+    @classmethod
+    def replace_non_letters_non_numbers_with_whitespace(cls, a_string): ...
+    strip: Incomplete
+    to_lower_case: Incomplete
+    to_upper_case: Incomplete
