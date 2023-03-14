87852f42117d3ed2b956d0c5770d92867927ee84
ryanhausen <rhausen@ucsc.edu>
2023-03-14 16:52:00 -0400

    benchmarking model 1 ready for testing on sciserver

--- src/models/kaggle_model1.py
+++ src/models/kaggle_model1.py
@@ -3,12 +3,13 @@ from functools import partial
 import glob
 from itertools import filterfalse
 import os
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
 import spacy
 import tensorflow as tf
+from tqdm import tqdm
 import transformers as tfs
 
 from src.data.repository import Repository
@@ -34,6 +35,7 @@ def validate(repository: Repository, config: Dict[str, Any]) -> None:
 class KaggleModel1(bm.Model):
     def __init__(self) -> None:
         self.nlp = spacy.load("en_core_web_sm")
+        self.model = None
 
     def train(
         self,
@@ -108,71 +110,112 @@ class KaggleModel1(bm.Model):
         return split_up
 
 
+    def is_special_token(token):
+        return (
+            token.startswith("[") and token.endswith("]")
+            or token.startswith("<") and token.endswith(">")
+        )
+
+
+    def merge_tokens_w_classifications(
+        self,
+        tokens:List[str],
+        token_should_be_merged:List[bool],
+        classifications:List[float]
+    ) -> List[Tuple[str, float]]:
+        merged = []
+        for token, do_merge, classification in zip(tokens, token_should_be_merged, classifications):
+            if do_merge:
+                merged[-1] = (merged[-1][0] + token, merged[-1][1])
+            else:
+                merged.append((token, classification))
+        return merged
+
+
+    def high_probablity_token_groups(
+        self,
+        tokens_classifications: List[Tuple[str, float]],
+        threshold:float=0.9,
+    ) -> List[List[Tuple[str, float]]]:
+
+        datasets = []
+        dataset = []
+        for token, score in tokens_classifications:
+            # REFACTOR HERE TO RECOVER SCORES
+            if score >= threshold:
+                dataset.append(token)
+            else:
+                if len(dataset) > 0:
+                    datasets.append(" ".join(dataset))
+                    dataset = []
+        if len(dataset) > 0:
+            datasets.append(" ".join(dataset))
+
+        return datasets
+
+
     def inference(self, config: Dict[str, Any], df: pd.DataFrame) -> pd.DataFrame:
 
-        model_config = tfs.AutoConfig.from_pretrained(config["model_tokenizer_name"])
-        model_config.output_attentions = True
-        model_config.output_hidden_states = True
+        if self.model is None:
+            model_config = tfs.AutoConfig.from_pretrained(config["model_tokenizer_name"])
+            model_config.output_attentions = True
+            model_config.output_hidden_states = True
 
-        ll_model = tfs.TFAutoModel.from_config(config=model_config)
-        model = MetricLearningModel(config=model_config, name="metric_learning_model")
-        model.main_model = ll_model
-        model.K = 3
+            ll_model = tfs.TFAutoModel.from_config(config=model_config)
+            model = MetricLearningModel(config=model_config, name="metric_learning_model")
+            model.main_model = ll_model
+            model.K = 3
 
-        tokenizer = tfs.AutoTokenizer.from_pretrained(config["model_tokenizer_name"])
-        mask_embedding = self.get_support_mask_embed(
-            config["support_mask_embedding_path"],
-            config["n_support_samples"],
-        ) # [n,     embed_dim]
+            self.tokenizer = tfs.AutoTokenizer.from_pretrained(config["model_tokenizer_name"])
+            self.tokenizer_special_tokens = set(self.tokenizer.special_tokens_map.values())
+            mask_embedding = self.get_support_mask_embed(
+                config["support_mask_embedding_path"],
+                config["n_support_samples"],
+            ) # [n,     embed_dim]
 
-        no_mask_embedding = self.get_support_mask_embed(
-            config["support_no_mask_embedding_path"],
-            config["n_support_samples"],
-        ) # [n,     embed_dim]
+            no_mask_embedding = self.get_support_mask_embed(
+                config["support_no_mask_embedding_path"],
+                config["n_support_samples"],
+            ) # [n,     embed_dim]
+
+            # empty mock values are passed to the model to build the tensorflow
+            # graph. We don't need the outputs
+            mock_input_ids = np.zeros([config["batch_size"], config["seq_len"]], dtype=np.int32)
+            mock_attention_mask = np.zeros([config["batch_size"], config["seq_len"]], dtype=np.int32)
+            _ = model(
+                [
+                    mock_input_ids,
+                    mock_attention_mask,
+                ],
+                training=True,
+                sequence_labels=None,
+                mask_embeddings=mask_embedding[:1, :],
+                nomask_embeddings=no_mask_embedding[:1, :],
+            )
+
+            weights_path = glob.glob(os.path.join(config["weights_path"], "*.h5"))[0]
+            model.load_weights(weights_path, by_name=True)
+            self.model = tf.function(model, experimental_relax_shapes=True)
 
 
-        slice_f = partial(self.slice_up_samples, config["seq_len"])
-        slice_dicts = df.apply(slice_f, axis=1)
-        sliced_df = pd.DataFrame(dict(
-            id = [d["id"] for d in slice_dicts],
-            text = [d["text"] for d in slice_dicts],
-        ))
-        # sliced = self.slice_up_samples(df, config["seq_len"])
-
-
-        query_dl = QueryDataLoader(sliced_df, batch_size=config["batch_size"])
-        test_dataloader = SupportQueryDataLoader(
-            sliced_df,
-            tokenizer=tokenizer,
-            batch_size=config["batch_size"],
-            is_train=False,
-            training_steps=len(query_dl),
-            query_dataloader=query_dl,
-            return_query_ids=True,
-        )
-        query_batch = test_dataloader.__getitem__(0)
-        print(query_batch["input_ids"].shape, query_batch["attention_mask"].shape)
-        _ = model(
-            [
-                query_batch["input_ids"][:1, ...],
-                query_batch["attention_mask"][:1, ...],
-            ],
-            training=True,
-            sequence_labels=None,
-            mask_embeddings=mask_embedding[:1, :],
-            nomask_embeddings=no_mask_embedding[:1, :],
-        )
-
-        weights_path = glob.glob(os.path.join(config["weights_path"], "*.h5"))[0]
-        model.load_weights(weights_path, by_name=True)
-        # model = tf.function(model, experimental_relax_shapes=True)
-
+        if config.get("is_roberta", False):
+            print("Merging tokens based on Roberta tokenizer")
+            should_merge = lambda t: not t.startswith("Ġ") and not t.startswith("<")
+            clean = lambda t: t.replace("Ġ", "")
+        else:
+            print("Merging tokens based on BERT tokenizer")
+            should_merge = lambda t: t.startswith("##")
+            clean = lambda t: t.replace("##", "")
 
         def infer_sample(text: str) -> str:
             sents = self._slice_up_text(config["seq_len"], text)
 
+            datasets = []
             for batch in spacy.util.minibatch(sents, config["batch_size"]):
-                tokenized_batch = tokenizer(
+                # batch is a list of string, the tokenizer will return a
+                # dictionary with at least the keys "input_ids" and
+                # "attention_mask" where the values are tensorflow tensors
+                tokenized_batch = self.tokenizer(
                     batch,
                     return_tensors="tf",
                     max_length=config["seq_len"],
@@ -180,13 +223,14 @@ class KaggleModel1(bm.Model):
                     padding=True,
                 )
 
+                # the embeddings are randomly sampled from the sets of embeddings
                 batch_mask_embeddings = mask_embedding[
                     np.random.choice(
                         np.arange(mask_embedding.shape[0]),
                         config["n_support_samples"],
                     ),
                     ...
-                ].mean(axis=0, keepdims=True) # [1, 1, embed_dim]
+                ].mean(axis=0, keepdims=True) # [1, embed_dim]
 
                 batch_no_mask_embeddings = no_mask_embedding[
                     np.random.choice(
@@ -194,14 +238,15 @@ class KaggleModel1(bm.Model):
                         config["n_support_samples"],
                     ),
                     ...
-                ].mean(axis=0, keepdims=True) # [1, 1, embed_dim]
+                ].mean(axis=0, keepdims=True) # [1, embed_dim]
+
 
                 (
-                    query_embeddings,
-                    query_mask_embeddings,
-                    query_nomask_embeddings,
+                    _,
+                    _,
+                    _,
                     attention_values
-                ) = model(
+                ) = self.model(
                     [
                         tokenized_batch["input_ids"],
                         tokenized_batch["attention_mask"],
@@ -213,40 +258,39 @@ class KaggleModel1(bm.Model):
                 )
 
                 tokens = list(map(
-                    tokenizer.convert_ids_to_tokens,
+                    self.tokenizer.convert_ids_to_tokens,
                     tokenized_batch["input_ids"].numpy()
                 ))
-
-
 
                 for sent, sent_classification in zip(
                     tokens,
                     attention_values.numpy()[...,0],
                 ):
                     assert len(sent) == len(sent_classification), f"Classification length mismatch {len(sent)} != {len(sent_classification)}"
-                    if np.any(sent_classification > 0.7):
-                        print(sent)
-                        print(sent_classification)
 
-                    # tokens_classifications = list(filterfalse(
-                    #     lambda x: is_special_token(x[0]),
-                    #     merge_tokens_w_classifications(
-                    #         list(map(clean, sent)),
-                    #         list(map(should_merge, sent)),
-                    #         sent_classification,
-                    #     )
-                    # ))
+                    tokens_classifications = list(filterfalse(
+                        lambda x: x[0] in self.tokenizer_special_tokens,
+                        self.merge_tokens_w_classifications(
+                            list(map(clean, sent)),
+                            list(map(should_merge, sent)),
+                            sent_classification,
+                        )
+                    ))
 
-                    # detections = high_probablity_token_groups(
-                    #     tokens_classifications,
-                    #     threshold=config.get("threshold", 0.9)
-                    # ) # List[List[Tuple[str, float]]]
+                    detections = self.high_probablity_token_groups(
+                        tokens_classifications,
+                        threshold=config.get("threshold", 0.9)
+                    ) # List[List[Tuple[str, float]]]
 
-                    # datasets.extend(detections)
-                # print("query_embeddings", query_embeddings.shape)
-                # print("query_mask_embeddings", query_mask_embeddings.shape)
+                    datasets.extend(detections)
 
-        df["model_prediction"] = df["text"].apply(infer_sample)
+            return "|".join(set(datasets))
+
+        if config.get("inference_progress_bar", False):
+            tqdm.pandas()
+            df["model_prediction"] = df["text"].progress_apply(infer_sample)
+        else:
+            df["model_prediction"] = df["text"].apply(infer_sample)
 
         return df
 
@@ -265,15 +309,31 @@ if __name__ == "__main__":
         def copy(self):
             return MockRepo(self.df.copy())
 
+    # biomed roberta model config
+    # config = dict(
+    #     support_mask_embedding_path = "models/kaggle_model1/sub_biomed_roberta/embeddings/support_mask_embeddings.npy",
+    #     support_no_mask_embedding_path = "models/kaggle_model1/sub_biomed_roberta/embeddings/support_nomask_embeddings.npy",
+    #     n_support_samples = 100,
+    #     model_tokenizer_name = "models/kaggle_model1/sub_biomed_roberta",
+    #     weights_path = "models/kaggle_model1/sub_biomed_roberta/embeddings",
+    #     batch_size = 128,
+    #     seq_len = 320,
+    #     is_roberta = True,
+    # )
+
+    # scibert model config
     config = dict(
-        support_mask_embedding_path = "models/kaggle_model1/sub_biomed_roberta/embeddings/support_mask_embeddings.npy",
-        support_no_mask_embedding_path = "models/kaggle_model1/sub_biomed_roberta/embeddings/support_nomask_embeddings.npy",
+        support_mask_embedding_path = "models/kaggle_model1/sub_scibert/embeddings/support_mask_embeddings.npy",
+        support_no_mask_embedding_path = "models/kaggle_model1/sub_scibert/embeddings/support_nomask_embeddings.npy",
         n_support_samples = 100,
-        model_tokenizer_name = "models/kaggle_model1/sub_biomed_roberta",
-        weights_path = "models/kaggle_model1/sub_biomed_roberta/embeddings",
+        model_tokenizer_name = "models/kaggle_model1/sub_scibert",
+        weights_path = "models/kaggle_model1/sub_scibert/embeddings",
         batch_size = 128,
         seq_len = 320,
+        is_roberta = False,
     )
+
+
 
     model = KaggleModel1()
 
