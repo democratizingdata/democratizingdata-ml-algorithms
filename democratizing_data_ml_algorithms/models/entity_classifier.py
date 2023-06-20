d9c6223606784b3ccd5c76aacff9baea085cee16
Ryan Hausen <ryan.hausen@gmail.com>
2023-06-20 13:51:55 -0400

    removed entity_classifier

--- democratizing_data_ml_algorithms/models/entity_classifier.py
+++
@@ -1,9 +0,0 @@
-# This model is a generalizaton of the approach from the second place submission.
-
-from itertools import chain
-from typing import Any, Dict, List
-
-import pandas as pd
-
-from democratizing_data_ml_algorithms.data.repository import Repository
-from democratizing_data_ml_algorithms.models.base_model import Model
